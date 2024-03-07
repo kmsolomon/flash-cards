e53f81cbdb7fa634d5d3cae439197851b6fa4a23
kmsolomon <3485058+kmsolomon@users.noreply.github.com>
2024-03-07 15:32:40 -0500

    Adding header/footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import "./main.css";
 
-import FlashCard from "./components/FlashCard/FlashCard";
-import { FlashCardType } from "./types";
+import { Outlet } from "react-router-dom";
+
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
 function App() {
-  const testCard: FlashCardType = {
-    id: "1234a",
-    question: "What is this project built with?",
-    answer: "React!",
-  };
-
   return (
     <>
-      <h1>Hello world</h1>
-      <FlashCard card={testCard} />
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+import "./Header.css";
+
+function Header() {
+  return <header>Flash cards</header>;
+}
+
+export default Header;
